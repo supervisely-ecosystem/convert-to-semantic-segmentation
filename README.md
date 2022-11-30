7e9f451bd5c6f011f54ecdefbd50f78634d816cd
ruslantau <ruslan042743@gmail.com>
2022-11-30 17:57:41 +0700

    transformation methods replaced to methods from SDK

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Instance segmentation to semantic segmentation
 
 ## Description
-...
-
-
+App convert all polygons and bitmaps from project datasets to new project and one dataset with bitmaps labels. 
--- src/main.py
+++ src/main.py
@@ -1,18 +1,7 @@
 import os
-import cv2
-import zlib
-import base64
 import numpy as np
 import supervisely as sly
 from dotenv import load_dotenv
-
-
-def base64_2_mask(s):
-    z = zlib.decompress(base64.b64decode(s))
-    n = np.frombuffer(z, np.uint8)
-    mask = cv2.imdecode(n, cv2.IMREAD_UNCHANGED)[:, :, 3].astype(bool)
-    return mask
-
 
 # init api for communicating with Supervisely Instance
 load_dotenv("local.env")
@@ -26,32 +15,30 @@ if workspace is None:
     print("you should put correct workspaceId value to local.env")
     raise ValueError(f"Workspace with id={workspace_id} not found")
 
-################################    Part 1    ######################################
-###################    create empty project and dataset    #########################
-################################    ------    ######################################
-
+# get or create new project
 initial_project = api.project.get_info_by_id(os.environ["modal.state.slyProjectId"])
 meta_json = api.project.get_meta(initial_project.id)
 initial_project_meta = sly.ProjectMeta.from_json(meta_json)
 
 sem_seg_project = api.project.get_info_by_name(653, name="Sem-seg")
 if not sem_seg_project:
-    # create empty project and dataset on server
     sem_seg_project = api.project.create(workspace.id, name="Sem-seg")
 
+# create new dataset
 sem_seg_dataset = api.dataset.create(
     sem_seg_project.id,
-    name="lemons SS",
+    name="lemons",
     change_name_if_conflict=True,
 )
 
-# export all classes with bitmap type
+# export all classes from initial project to new project with bitmap type
 objects_dict = {
     k: sly.ObjClass(k, sly.Bitmap) for k in initial_project_meta.obj_classes.keys()
 }
 sem_seg_project_meta = sly.ProjectMeta(obj_classes=list(objects_dict.values()))
 api.project.update_meta(sem_seg_project.id, sem_seg_project_meta.to_json())
 print(f"Dataset has been sucessfully created, id={sem_seg_dataset.id}")
+
 
 datasets = api.dataset.get_list(initial_project.id)
 for dataset in datasets:
@@ -62,43 +49,38 @@ for dataset in datasets:
     for batch in sly.batched(images):
         img_names, image_ids, img_metas = zip(*((x.name, x.id, x.meta) for x in batch))
         annotations = api.annotation.download_json_batch(dataset.id, image_ids)
+
         new_img_infos = api.image.upload_ids(
             sem_seg_dataset.id, img_names, image_ids, metas=img_metas
         )
         new_img_ids = [x.id for x in new_img_infos]
 
         for new_img_id, image, ann_json in zip(new_img_ids, batch, annotations):
+            image_annotations = sly.Annotation.from_json(ann_json, initial_project_meta)
+
             mask_images = {}
-            for anno in ann_json["objects"]:
-                current_class = anno["classTitle"]
-                geometry_type = anno["geometryType"]
+            for index, anno in enumerate(image_annotations.labels, start=1):
+                current_class = anno.obj_class.name
+                geometry_type = anno.obj_class.geometry_type
 
                 if mask_images.get(current_class) is None and geometry_type in (
-                    "bitmap",
-                    "polygon",
+                    sly.Bitmap,
+                    sly.Polygon,
                 ):
-                    mask_images[current_class] = np.zeros((image.height, image.width))
-
-                if geometry_type == "polygon":
-                    mask_images[current_class] = cv2.fillPoly(
-                        mask_images[current_class],
-                        pts=np.array([anno["points"]["exterior"]]),
-                        color=1,
-                    )
-                elif geometry_type == "bitmap":
-                    bitmap = base64_2_mask(anno["bitmap"]["data"])
-                    x0, y0 = anno["bitmap"]["origin"]
-                    x1, y1 = x0 + bitmap.shape[1], y0 + bitmap.shape[0]
-                    mask_images[current_class][y0:y1, x0:x1] = np.clip(
-                        mask_images[current_class][y0:y1, x0:x1] + bitmap, 0, 1
+                    mask_images[current_class] = np.zeros(
+                        image_annotations.img_size, bool
                     )
 
+                if geometry_type is sly.Polygon:
+                    anno.convert(sly.ObjClass(current_class, sly.Bitmap))
+                    anno.draw(mask_images[current_class], 1)
+                elif geometry_type is sly.Bitmap:
+                    anno.draw(mask_images[current_class], 1)
                 else:
                     continue
 
             image_annos = []
             for mask_class, mask_arr in mask_images.items():
-                # supports masks with values (0, 1) or (0, 255) or (False, True)
                 mask = sly.Bitmap(mask_arr)
                 label = sly.Label(geometry=mask, obj_class=objects_dict[mask_class])
                 image_annos.append(label)
